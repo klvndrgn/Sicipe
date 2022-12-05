53c7a62cb16b68aa3502943aea8598c38bab9c39
klvndrgn <klvndrgnn@gmail.com>
2022-12-05 22:48:24 +0700

    Feeds Initialize

--- lib/feeds_page.dart
+++ lib/feeds_page.dart
@@ -1,2 +1,26 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:sicipe/home_page.dart';
+
+class FeedScreen extends StatefulWidget {
+  const FeedScreen({super.key});
+
+  @override
+  State<FeedScreen> createState() => _FeedScreenState();
+}
+
+class _FeedScreenState extends State<FeedScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'Feeds',
+          style: GoogleFonts.jost(
+              color: Colors.white, fontSize: 20, fontWeight: FontWeight.w500),
+        ),
+        backgroundColor: Colors.deepOrange,
+      ),
+    );
+  }
+}
