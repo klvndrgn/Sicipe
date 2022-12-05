76e28e9f2b4ba178843d25f796ee357deb6b22f5
Eric2031010 <73868408+Eric2031010@users.noreply.github.com>
2022-12-05 23:17:18 +0700

    ButtomNavigation

--- lib/feeds_page.dart
+++ lib/feeds_page.dart
@@ -1,2 +1,218 @@
 import 'package:flutter/material.dart';
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:sicipe/home_page.dart';
+import 'package:sicipe/login_page.dart';
+import 'package:sicipe/register_page.dart';
+import 'package:sicipe/search.dart';
+
+class feeds_page extends StatefulWidget {
+  const feeds_page({super.key});
+
+  @override
+  State<feeds_page> createState() => _feeds_pageState();
+}
+
+class _feeds_pageState extends State<feeds_page> {
+  int _currentIndex = 0;
+  List _screens = [HomeScreen(), RegisterPage(), LoginScreen(), feeds_page()];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _currentIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+          backgroundColor: Colors.deepOrange,
+          title: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 12.0),
+              child: Text('Profile Saya',
+                  style: GoogleFonts.jost(color: Colors.white, fontSize: 20)))),
+      body: SingleChildScrollView(
+        child: Column(children: [
+          SizedBox(height: 35),
+          Row(
+            children: [
+              SizedBox(width: 26),
+              Image.asset('assets/icons/profile-picture.png'),
+              SizedBox(width: 29),
+              Text('Kimberly',
+                  style: (GoogleFonts.jost(color: Colors.black, fontSize: 18))),
+              SizedBox(width: 165),
+              Icon(Icons.arrow_forward_ios,
+                  size: 19, color: Color.fromARGB(255, 207, 203, 203))
+            ],
+          ),
+          SizedBox(height: 20),
+          Divider(color: Colors.grey, thickness: 0.5),
+          SizedBox(height: 8),
+          Row(
+            children: [
+              Column(
+                children: [
+                  Row(
+                    children: [
+                      SizedBox(width: 6),
+                      Text('Saldo Saya',
+                          style: (GoogleFonts.jost(
+                              color: Colors.black, fontSize: 16))),
+                    ],
+                  ),
+                  SizedBox(height: 10),
+                  Row(
+                    children: [
+                      SizedBox(width: 26),
+                      Text('Rp.810.500',
+                          style: (GoogleFonts.jost(
+                              color: Colors.deepOrange,
+                              fontWeight: FontWeight.w500,
+                              fontSize: 18)))
+                    ],
+                  )
+                ],
+              ),
+              SizedBox(width: 45),
+              Column(
+                children: [
+                  ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      minimumSize: Size(95, 33),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(5),
+                      ),
+                      backgroundColor: Colors.deepOrange,
+                    ),
+                    onPressed: () {
+                      Navigator.pushReplacement(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => LoginScreen()));
+                    },
+                    child: Text(
+                      'Top Up',
+                      style: GoogleFonts.jost(
+                          color: Colors.white,
+                          fontSize: 13,
+                          fontWeight: FontWeight.w500),
+                    ),
+                  )
+                ],
+              ),
+              SizedBox(width: 20),
+              Column(
+                children: [
+                  ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      minimumSize: Size(95, 33),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(5),
+                      ),
+                      backgroundColor: Colors.deepOrange,
+                    ),
+                    onPressed: () {
+                      Navigator.pushReplacement(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => LoginScreen()));
+                    },
+                    child: Text(
+                      'Tarik Saldo',
+                      style: GoogleFonts.jost(
+                          color: Colors.white,
+                          fontSize: 13,
+                          fontWeight: FontWeight.w500),
+                    ),
+                  )
+                ],
+              )
+            ],
+          ),
+          SizedBox(height: 8),
+          Divider(color: Colors.grey, thickness: 0.5),
+          SizedBox(height: 20),
+          Row(
+            children: [
+              SizedBox(width: 26),
+              Text('Resep Saya',
+                  style: (GoogleFonts.jost(color: Colors.black, fontSize: 16))),
+              SizedBox(width: 230),
+              Icon(Icons.arrow_forward_ios,
+                  size: 21, color: Color.fromARGB(255, 207, 203, 203))
+            ],
+          ),
+          SizedBox(height: 20),
+          Divider(color: Colors.grey, thickness: 0.5),
+          SizedBox(height: 20),
+          Row(
+            children: [
+              SizedBox(width: 26),
+              Text('Feeds Saya',
+                  style: (GoogleFonts.jost(color: Colors.black, fontSize: 16))),
+              SizedBox(width: 230),
+              Icon(Icons.arrow_forward_ios,
+                  size: 21, color: Color.fromARGB(255, 207, 203, 203))
+            ],
+          ),
+          SizedBox(height: 20),
+          Divider(color: Colors.grey, thickness: 0.5),
+          SizedBox(height: 20),
+          Row(
+            children: [
+              SizedBox(width: 26),
+              Text('Keamanan Akun',
+                  style: (GoogleFonts.jost(color: Colors.black, fontSize: 16))),
+              SizedBox(width: 198),
+              Icon(Icons.arrow_forward_ios,
+                  size: 21, color: Color.fromARGB(255, 207, 203, 203))
+            ],
+          ),
+          SizedBox(height: 20),
+          Divider(color: Colors.grey, thickness: 0.5),
+        ]),
+      ),
+    );
+  }
+}
+
+class ItemKategoriResep extends StatelessWidget {
+  const ItemKategoriResep({
+    Key? key,
+    required this.title,
+    required this.icon,
+  }) : super(key: key);
+
+  final String title;
+  final String icon;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.center,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Container(
+          width: 82,
+          height: 28,
+          child: Image.asset(
+            icon,
+          ),
+        ),
+        SizedBox(
+          width: 82,
+          child: Text(
+            title,
+            style: GoogleFonts.jost(
+                color: Colors.black, fontSize: 15, fontWeight: FontWeight.w500),
+            overflow: TextOverflow.ellipsis,
+            maxLines: 2,
+            textAlign: TextAlign.center,
+          ),
+        )
+      ],
+    );
+  }
+}
