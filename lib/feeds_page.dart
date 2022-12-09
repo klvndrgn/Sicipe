d594de0b4e5cb1fbe9f0658a96f4bf19b98ab8dd
klvndrgn <klvndrgnn@gmail.com>
2022-12-10 06:44:10 +0700

    Added More Feeds Content

--- lib/feeds_page.dart
+++ lib/feeds_page.dart
@@ -23,6 +23,7 @@ class _FeedScreenState extends State<FeedScreen> {
       body: ListView(
         physics: AlwaysScrollableScrollPhysics(),
         children: <Widget>[
+          // Feeds 1
           SizedBox(height: 20),
           Padding(
             padding:
@@ -76,7 +77,7 @@ class _FeedScreenState extends State<FeedScreen> {
                               color: Colors.black, fontSize: 14),
                         ),
                         subtitle: Text(
-                          '1 HOURS AGO',
+                          '30 minutes ago',
                           style: GoogleFonts.jost(
                               color: Colors.grey.shade400, fontSize: 10),
                         ),
@@ -140,7 +141,126 @@ class _FeedScreenState extends State<FeedScreen> {
                 ),
               ]),
             ),
-          )
+          ),
+          // Feeds 2
+          SizedBox(height: 20),
+          Padding(
+            padding:
+                const EdgeInsets.symmetric(horizontal: 10.0, vertical: 5.0),
+            child: Container(
+              width: double.infinity,
+              height: 400.0,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(25),
+                color: Colors.white,
+                boxShadow: [
+                  BoxShadow(
+                      color: Colors.black45,
+                      offset: Offset(0, 2),
+                      blurRadius: 8.0),
+                ],
+              ),
+              child: Column(children: <Widget>[
+                Padding(
+                  padding: EdgeInsets.symmetric(vertical: 10.0),
+                  child: Column(
+                    children: <Widget>[
+                      ListTile(
+                        leading: Container(
+                          margin: EdgeInsets.all(9.0),
+                          width: 40.0,
+                          height: 40.0,
+                          decoration:
+                              BoxDecoration(shape: BoxShape.circle, boxShadow: [
+                            BoxShadow(
+                              color: Colors.black45,
+                              offset: Offset(0, 2),
+                              blurRadius: 6.0,
+                            ),
+                          ]),
+                          child: CircleAvatar(
+                            child: ClipOval(
+                              child: Image(
+                                height: 40.0,
+                                width: 40.0,
+                                image: AssetImage(
+                                    'assets/icons/profile-picture2.png'),
+                                fit: BoxFit.cover,
+                              ),
+                            ),
+                          ),
+                        ),
+                        title: Text(
+                          'George Michael',
+                          style: GoogleFonts.jost(
+                              color: Colors.black, fontSize: 14),
+                        ),
+                        subtitle: Text(
+                          '1 hours ago',
+                          style: GoogleFonts.jost(
+                              color: Colors.grey.shade400, fontSize: 10),
+                        ),
+                        trailing: IconButton(
+                          color: Colors.deepOrange,
+                          icon: Icon(Icons.more_horiz),
+                          onPressed: () => print('More'),
+                        ),
+                      ),
+                      Row(
+                        children: [
+                          Expanded(
+                            child: Container(
+                              margin: EdgeInsets.only(
+                                  left: 15, right: 15, bottom: 5),
+                              child: Text(
+                                'For my last meal here on Earth, I want a nicely cooked steak!!',
+                                style: GoogleFonts.jost(
+                                    color: Colors.black, fontSize: 14),
+                              ),
+                            ),
+                          )
+                        ],
+                      ),
+                      Container(
+                        margin: EdgeInsets.zero,
+                        width: double.infinity,
+                        height: 200.0,
+                        decoration: BoxDecoration(
+                          image: DecorationImage(
+                            image: AssetImage('assets/steak.png'),
+                            fit: BoxFit.cover,
+                          ),
+                        ),
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Row(
+                            children: [
+                              Row(
+                                children: [
+                                  IconButton(
+                                      onPressed: () {},
+                                      icon: Icon(Icons.favorite_border))
+                                ],
+                              ),
+                              Row(
+                                children: [
+                                  IconButton(
+                                      onPressed: () {},
+                                      icon: Icon(Icons.comment_outlined))
+                                ],
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              ]),
+            ),
+          ),
         ],
       ),
       floatingActionButton: FloatingActionButton(
