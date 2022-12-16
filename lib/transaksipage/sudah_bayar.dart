d959f60c12bba97a0102d87422b3346872cc6cb8
klvndrgn <klvndrgnn@gmail.com>
2022-12-16 16:05:04 +0700

    Transaksi Page

---
+++ lib/transaksipage/sudah_bayar.dart
@@ -0,0 +1,106 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:sicipe/detail_akun.dart';
+
+class SudahBayar extends StatefulWidget {
+  const SudahBayar({super.key});
+
+  @override
+  State<SudahBayar> createState() => _SudahBayarState();
+}
+
+class _SudahBayarState extends State<SudahBayar> {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Container(
+          height: 120,
+          margin: EdgeInsets.symmetric(horizontal: 15, vertical: 10),
+          padding: EdgeInsets.all(10),
+          decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(10),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.black45,
+                  offset: Offset(0, 2),
+                  blurRadius: 5.0,
+                ),
+              ]),
+          child: Row(children: [
+            Container(
+              height: 80,
+              width: 80,
+              margin: EdgeInsets.only(right: 15),
+              child: Image.asset('assets/steak2.png'),
+            ),
+            Padding(
+              padding: EdgeInsets.symmetric(vertical: 10),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    'Resep Steak',
+                    style: GoogleFonts.jost(
+                        color: Colors.black,
+                        fontSize: 18,
+                        fontWeight: FontWeight.w500),
+                  ),
+                  Text(
+                    'George Michael',
+                    style: GoogleFonts.jost(
+                        color: Colors.black,
+                        fontSize: 12,
+                        fontWeight: FontWeight.w500),
+                  ),
+                  Text(
+                    'Rp 100.000',
+                    style: GoogleFonts.jost(
+                        color: Colors.deepOrange,
+                        fontSize: 18,
+                        fontWeight: FontWeight.w500),
+                  )
+                ],
+              ),
+            ),
+            SizedBox(width: 50),
+            Padding(
+              padding: EdgeInsets.symmetric(vertical: 5),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.end,
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      minimumSize: Size(15, 30),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(5),
+                      ),
+                      backgroundColor: Colors.deepOrange,
+                    ),
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => const detail_akun()),
+                      );
+                    },
+                    child: Text(
+                      'Lihat Resep',
+                      style: GoogleFonts.jost(
+                          color: Colors.white,
+                          fontSize: 13,
+                          fontWeight: FontWeight.w500),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ]),
+        ),
+      ],
+    );
+  }
+}
