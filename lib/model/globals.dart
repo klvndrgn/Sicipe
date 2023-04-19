2fe9aedb13a10bf7172f719225911b0829e784dc
Kevin Charles <kevincharlesid@gmail.com>
2023-04-19 11:19:20 +0700

    edit profile

--- lib/model/globals.dart
+++ lib/model/globals.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-const String baseURL = "http://10.0.2.2:8000/api/"; //emulator localhost
+const String serverBaseURL = "http://10.0.2.2:8000"; //emulator localhost
+const String baseURL = serverBaseURL + "/api/"; //emulator localhost
 const Map<String, String> headers = {"Content-Type": "application/json"};
 
 errorSnackBar(BuildContext context, String text) {
--- lib/profile_saya.dart
+++ lib/profile_saya.dart
@@ -1,11 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 import 'package:google_fonts/google_fonts.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:sicipe/detail_akun.dart';
 import 'package:sicipe/feeds_saya.dart';
+import 'package:sicipe/model/auth_services.dart';
 import 'package:sicipe/resep_saya.dart';
 import 'package:sicipe/keamanan_akun.dart';
 import 'package:sicipe/payment.dart';
 import 'package:sicipe/withdrawal.dart';
+import 'dart:convert';
+
+import 'package:sicipe/model/globals.dart';
 
 class profile_saya extends StatefulWidget {
   const profile_saya({super.key});
@@ -15,201 +21,309 @@ class profile_saya extends StatefulWidget {
 }
 
 class _profile_sayaState extends State<profile_saya> {
+  var myProfile;
+
+  bool _isInit = true;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _isInit = true;
+  }
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+
+    if (_isInit) {
+      _isInit = false;
+      _asyncMethod();
+    }
+  }
+
+  _asyncMethod() async {
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+
+    final String token = prefs.getString('token') ?? "";
+
+    http.Response response = await AuthServices.me(token);
+    Map<String, dynamic> decodedData = json.decode(response.body);
+
+    setState(() {
+      myProfile = decodedData;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-          backgroundColor: Colors.deepOrange,
-          title: Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 10.0),
-              child: Text('Profile Saya',
+    void _onOtherScreenPopped(dynamic result) {
+      setState(() {
+        _isInit = true;
+      });
+    }
+
+    if (_isInit) {
+      _isInit = false;
+      _asyncMethod();
+    }
+
+    return (myProfile == null || _isInit)
+        ? Center(
+            child: CircularProgressIndicator(),
+          )
+        : Scaffold(
+            appBar: AppBar(
+              backgroundColor: Colors.deepOrange,
+              title: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 10.0),
+                child: Text(
+                  'Profile Saya',
                   style: GoogleFonts.jost(
                       color: Colors.white,
                       fontSize: 20,
-                      fontWeight: FontWeight.w500)))),
-      body: SingleChildScrollView(
-        child: Column(children: [
-          Padding(
-            padding: const EdgeInsets.only(left: 28, top: 35),
-            child: Row(
-              children: [
-                Image.asset('assets/icons/profile-picture.png'),
-                SizedBox(width: 29),
-                Text('Kimberly',
-                    style:
-                        (GoogleFonts.jost(color: Colors.black, fontSize: 18))),
-                SizedBox(width: 165),
-                IconButton(
-                    icon: Icon(Icons.arrow_forward_ios,
-                        size: 21, color: Color.fromARGB(255, 207, 203, 203)),
-                    onPressed: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) => const detail_akun()),
-                      );
-                    }),
-              ],
+                      fontWeight: FontWeight.w500),
+                ),
+              ),
             ),
-          ),
-          SizedBox(height: 20),
-          Divider(color: Colors.grey, thickness: 0.3),
-          SizedBox(height: 5),
-          Row(
-            children: [
-              Column(
-                children: [
-                  Row(
+            body: SingleChildScrollView(
+              child: Column(children: [
+                new InkWell(
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => detail_akun(myProfile),
+                      ),
+                    ).then((result) => _onOtherScreenPopped(result));
+                  },
+                  child: Padding(
+                    padding: const EdgeInsets.all(20),
+                    child: Row(
+                      children: [
+                        Image.network(
+                          serverBaseURL + myProfile["photo_profile"],
+                          width: 48,
+                          height: 48,
+                          errorBuilder: (BuildContext context, Object exception,
+                              StackTrace? stackTrace) {
+                            return Icon(
+                              Icons.account_circle_sharp,
+                              size: 48,
+                              color: Colors.deepOrange,
+                            );
+                          },
+                        ),
+                        Expanded(
+                          child: Padding(
+                            padding: EdgeInsets.only(left: 20, right: 20),
+                            child: Text(
+                              myProfile["username_pengguna"],
+                              style: (GoogleFonts.jost(
+                                  color: Colors.black, fontSize: 18)),
+                            ),
+                          ),
+                        ),
+                        Icon(
+                          Icons.arrow_forward_ios,
+                          size: 21,
+                          color: Color.fromARGB(255, 207, 203, 203),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                Divider(color: Colors.grey, thickness: 0.5),
+                Padding(
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+                  child: Row(
                     children: [
-                      SizedBox(width: 8),
-                      Text('Saldo Saya',
-                          style: (GoogleFonts.jost(
-                              color: Colors.black, fontSize: 16))),
+                      Expanded(
+                        child: Container(
+                          child: Column(
+                            children: [
+                              Text(
+                                'Saldo Saya',
+                                style: GoogleFonts.jost(
+                                  color: Colors.black,
+                                  fontSize: 16,
+                                  fontWeight: FontWeight.w400,
+                                ),
+                              ),
+                              SizedBox(height: 2),
+                              Text(
+                                'Rp.' + (myProfile["sisa_saldo"] ?? "0"),
+                                style: GoogleFonts.jost(
+                                  color: Colors.deepOrange,
+                                  fontWeight: FontWeight.w500,
+                                  fontSize: 18,
+                                ),
+                              ),
+                            ],
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                          ),
+                        ),
+                      ),
+                      Row(
+                        children: [
+                          ElevatedButton(
+                            style: ElevatedButton.styleFrom(
+                              minimumSize: Size(95, 33),
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(5),
+                              ),
+                              backgroundColor: Colors.deepOrange,
+                            ),
+                            onPressed: () {
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) =>
+                                        const PaymentScreen()),
+                              );
+                            },
+                            child: Text(
+                              'Top Up',
+                              style: GoogleFonts.jost(
+                                color: Colors.white,
+                                fontSize: 13,
+                                fontWeight: FontWeight.w500,
+                              ),
+                            ),
+                          ),
+                          Container(
+                            margin: EdgeInsets.only(left: 20),
+                            child: ElevatedButton(
+                              style: ElevatedButton.styleFrom(
+                                minimumSize: Size(95, 33),
+                                shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(5),
+                                ),
+                                backgroundColor: Colors.deepOrange,
+                              ),
+                              onPressed: () {
+                                Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                      builder: (context) => WithdrawScreen()),
+                                );
+                              },
+                              child: Text(
+                                'Tarik Saldo',
+                                style: GoogleFonts.jost(
+                                  color: Colors.white,
+                                  fontSize: 13,
+                                  fontWeight: FontWeight.w500,
+                                ),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
                     ],
                   ),
-                  SizedBox(height: 5),
-                  Row(
-                    children: [
-                      SizedBox(width: 26),
-                      Text('Rp.810.500',
-                          style: (GoogleFonts.jost(
-                              color: Colors.deepOrange,
-                              fontWeight: FontWeight.w500,
-                              fontSize: 18)))
-                    ],
-                  )
-                ],
-              ),
-              SizedBox(width: 45),
-              Column(
-                children: [
-                  ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                      minimumSize: Size(95, 33),
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(5),
-                      ),
-                      backgroundColor: Colors.deepOrange,
-                    ),
-                    onPressed: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) => const PaymentScreen()),
-                      );
-                    },
-                    child: Text(
-                      'Top Up',
-                      style: GoogleFonts.jost(
-                          color: Colors.white,
-                          fontSize: 13,
-                          fontWeight: FontWeight.w500),
-                    ),
-                  )
-                ],
-              ),
-              SizedBox(width: 22),
-              Column(
-                children: [
-                  ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                      minimumSize: Size(95, 33),
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(5),
-                      ),
-                      backgroundColor: Colors.deepOrange,
-                    ),
-                    onPressed: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) => WithdrawScreen()),
-                      );
-                    },
-                    child: Text(
-                      'Tarik Saldo',
-                      style: GoogleFonts.jost(
-                          color: Colors.white,
-                          fontSize: 13,
-                          fontWeight: FontWeight.w500),
-                    ),
-                  )
-                ],
-              )
-            ],
-          ),
-          Divider(color: Colors.grey, thickness: 0.3),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Padding(
-                padding: const EdgeInsets.only(top: 5, bottom: 5, left: 28),
-                child: Text('Resep Saya',
-                    style:
-                        (GoogleFonts.jost(color: Colors.black, fontSize: 16))),
-              ),
-              IconButton(
-                  icon: Icon(Icons.arrow_forward_ios,
-                      size: 21, color: Color.fromARGB(255, 207, 203, 203)),
-                  onPressed: () {
+                ),
+                Divider(color: Colors.grey, thickness: 0.5),
+                new InkWell(
+                  onTap: () {
                     Navigator.push(
                       context,
                       MaterialPageRoute(
-                          builder: (context) => const resep_saya()),
+                        builder: (context) => const resep_saya(),
+                      ),
                     );
-                  }),
-            ],
-          ),
-          Divider(color: Colors.grey, thickness: 0.3),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Padding(
-                padding: const EdgeInsets.only(top: 5, bottom: 5, left: 28),
-                child: Text('Feeds Saya',
-                    style:
-                        (GoogleFonts.jost(color: Colors.black, fontSize: 16))),
-              ),
-              IconButton(
-                  icon: Icon(Icons.arrow_forward_ios,
-                      size: 21, color: Color.fromARGB(255, 207, 203, 203)),
-                  onPressed: () {
-                    Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) => const FeedsSayaScreen()));
-                  }),
-            ],
-          ),
-          SizedBox(height: 5),
-          Divider(color: Colors.grey, thickness: 0.3),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Padding(
-                padding: const EdgeInsets.only(top: 5, bottom: 5, left: 28),
-                child: Text('Keamanan Akun',
-                    style:
-                        (GoogleFonts.jost(color: Colors.black, fontSize: 16))),
-              ),
-              IconButton(
-                  icon: Icon(Icons.arrow_forward_ios,
-                      size: 21, color: Color.fromARGB(255, 207, 203, 203)),
-                  onPressed: () {
+                  },
+                  child: Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 20, vertical: 15),
+                    child: Row(
+                      children: [
+                        Expanded(
+                          child: Text(
+                            'Resep Saya',
+                            style: (GoogleFonts.jost(
+                              color: Colors.black,
+                              fontSize: 16,
+                            )),
+                          ),
+                        ),
+                        Icon(
+                          Icons.arrow_forward_ios,
+                          size: 21,
+                          color: Color.fromARGB(255, 207, 203, 203),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                Divider(color: Colors.grey, thickness: 0.5),
+                new InkWell(
+                  onTap: () {
                     Navigator.push(
                       context,
                       MaterialPageRoute(
-                          builder: (context) => const keamanan_akun()),
+                        builder: (context) => const FeedsSayaScreen(),
+                      ),
                     );
-                  }),
-            ],
-          ),
-          Divider(color: Colors.grey, thickness: 0.3),
-        ]),
-      ),
-
-      // Navigation
-    );
+                  },
+                  child: Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 20, vertical: 15),
+                    child: Row(
+                      children: [
+                        Expanded(
+                          child: Text(
+                            'Feed Saya',
+                            style: (GoogleFonts.jost(
+                              color: Colors.black,
+                              fontSize: 16,
+                            )),
+                          ),
+                        ),
+                        Icon(
+                          Icons.arrow_forward_ios,
+                          size: 21,
+                          color: Color.fromARGB(255, 207, 203, 203),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                Divider(color: Colors.grey, thickness: 0.5),
+                new InkWell(
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => const keamanan_akun(),
+                      ),
+                    );
+                  },
+                  child: Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 20, vertical: 15),
+                    child: Row(
+                      children: [
+                        Expanded(
+                          child: Text(
+                            'Keamanan Akun',
+                            style: (GoogleFonts.jost(
+                              color: Colors.black,
+                              fontSize: 16,
+                            )),
+                          ),
+                        ),
+                        Icon(
+                          Icons.arrow_forward_ios,
+                          size: 21,
+                          color: Color.fromARGB(255, 207, 203, 203),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                Divider(color: Colors.grey, thickness: 0.5),
+              ]),
+            ),
+          );
   }
 }
 
